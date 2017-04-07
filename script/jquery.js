99f39e51959de7c9ce1d6e3c286e26537c25a6af
Jonathan Sebastian <jojosebseb@gmail.com>
2017-04-07 15:54:36 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -15,12 +15,14 @@ var addImage = $('#addImage');
 
 var slideParent = $('#slideParent');
 
-var xVal, yVal, colorVal, bgImgVal, textId, textSync;
+var xVal, yVal, colorVal, bgImgVal, textId, textSync, imgSync;
 
 
 
 var textIds = 0;
+var imgIds = 0;
 textSync = 'text'+textIds;
+textSync = 'img'+imgIds;
 
 
 var textEditor = '<div class=textEditor id='+textSync+'> <div class="drop-handle section-title">Your Text Here</div><div class="flex jo-drop"> <div class=form-group> <span class=form-label>Your Text :</span> <textarea class="fill-textarea custom-text"></textarea> </div><div class="form-group w50"> <span class=form-label>X-axis :</span> <input type="text" class="fill-input w50" value="50%"> </div><div class="form-group w50"> <span class=form-label>Y-axis :</span> <input type="text" class="fill-input w50" value="50%"> </div></div></div>'
@@ -52,17 +54,26 @@ bgImgInput.on('input', function() {
 });
 
 
+
 addText.on('click', function(){
     textIds++;
     textSync = 'text'+textIds;
     $(this).parents('#sideMountController').find('.drop-handle').addClass('hide');
     $(this).parents('#sideMountController').find('.jo-drop').addClass('hide');
-    $(this).parent().parent().append('<div class=textEditor id='+textSync+'> <div class="drop-handle section-title">Your Text Here</div><div class="flex jo-drop"> <div class=form-group> <span class=form-label>Your Text :</span> <textarea class="fill-textarea custom-text"></textarea> </div><div class="form-group w50"> <span class=form-label>X-axis :</span> <input type="text" class="fill-input w50 x-axis" value="50%"> </div><div class="form-group w50"> <span class=form-label>Y-axis :</span> <input type="text" class="fill-input w50 y-axis" value="50%"> </div></div></div>');
-    slideParent.append(' <div class="default-text jo-drag '+textSync+'"> Your Text Here </div>');
+    $(this).parent().parent().append(' <div class=textEditor id='+textSync+'> <div class="drop-handle section-title">Your Text Here</div><div class="flex jo-drop"> <div class=form-group> <span class=form-label>Your Text :</span> <textarea class="fill-textarea custom-text">Your Text Here</textarea> </div><div class="form-group w50"> <span class=form-label>X-axis :</span> <input type="text" class="fill-input w50 x-axis" value="50%"> </div><div class="form-group w50"> <span class=form-label>Y-axis :</span> <input type="text" class="fill-input w50 y-axis" value="50%"> </div><div class="form-group"> <div class="form-btn save save-mod"> Save </div><div class="form-btn del delete-mod"> Delete </div></div></div></div>');
+
+    slideParent.append(' <div class="default-text jo-drag snapped-middle '+textSync+'"> Your Text Here </div>');
 
     // var textEditor = '<div class=textEditor id='+textSync+'> <div class="drop-handle section-title">Your Text Here</div><div class="flex jo-drop"> <div class=form-group> <span class=form-label>Your Text :</span> <textarea class="fill-textarea custom-text"></textarea> </div><div class="form-group w50"> <span class=form-label>X-axis :</span> <input type="text" class="fill-input w50" value="50%"> </div><div class="form-group w50"> <span class=form-label>Y-axis :</span> <input type="text" class="fill-input w50" value="50%"> </div></div></div>'
     // var defaultText = ' <div class="default-text '+textSync+'"> Your Text Here </div>'
+})
 
+addImage.on('click', function(){
+    imgIds++;
+    imgSync = 'img'+imgIds;
+    $(this).parents('#sideMountController').find('.drop-handle').addClass('hide');
+    $(this).parents('#sideMountController').find('.jo-drop').addClass('hide');
+    $('#sideMountController').append(' <div class="imgEditor" id="imgMo"> <div class="drop-handle section-title">Your Text Here</div><div class="flex jo-drop"> <div class=form-group> <span class=form-label>Image Name :</span> <input type="text" class="fill-input w50 img-name"> </div><div class=form-group> <span class=form-label>Select File :</span> <input type="file" class="fill-input w50 img-src-input"> </div></div></div>')
 })
 
 $('#parentContainer').on("click", ".drop-handle", function() {
@@ -70,11 +81,22 @@ $('#parentContainer').on("click", ".drop-handle", function() {
     $(this).parent().find('.jo-drop').toggleClass('hide');
 });
 
+$('#parentContainer').on("click", ".delete-mod", function() {
+    $(this).parents('.textEditor').remove();
+    textId = $(this).parents('.textEditor').attr('id');
+    $('.'+textId).remove();
+});
+
+
+
 
 $('#parentContainer').on("input", ".custom-text", function() {
     $(this).parents('.textEditor').find('.section-title').html($(this).val())
     textId = $(this).parents('.textEditor').attr('id');
     $('.'+textId).html($(this).val());
+    $('.'+textId).css({
+        width: 'auto'
+    })
 
 });
 
@@ -85,6 +107,8 @@ $('#parentContainer').on("mouseenter", ".textEditor", function() {
 $('#parentContainer').on("mouseleave", ".textEditor", function() {
     $('.default-text').removeClass('hover');
 });
+
+
 
 $('#parentContainer').on("input", ".x-axis", function() {
     textId = $(this).parents('.textEditor').attr('id');
@@ -155,19 +179,30 @@ var xxx, yyy, curClass, outerW;
                     top: e.pageY + pos_y - drg_h,
                     left: e.pageX + pos_x - drg_w
                 });
-
+                $selected.removeClass('snapped-middle');
                 xxx = $selected.position().left;
                 yyy = $selected.position().top;
                 curClass = $selected.attr('class').split(' ').pop();
                 $('#'+curClass).find('.x-axis').val(Math.floor(xxx) + 'px');
                 $('#'+curClass).find('.y-axis').val(Math.floor(yyy) + 'px');
 
+                $('#'+curClass).find('.section-title').addClass('affected');
+
+                $('#'+curClass).find('.section-title').addClass('affected');
+
+                // $('#'+curClass).find('.jo-drop').removeClass('hide');
+                // $('#'+curClass).find('.drop-handle').removeClass('hide');
+
+
 
 
             }).on("mouseup", function() {
+                // curClass = $selected.attr('class').split(' ').pop();
+                $('#'+curClass).find('.section-title').removeClass('affected');
                 $(this).off("mousemove"); // Unbind events from document
                 if ($selected !== null) {
                     $selected.removeClass(opt.draggableClass);
+
                     $selected = null;
                 }
             });
@@ -196,4 +231,33 @@ $('#parentContainer').on("mouseenter", ".default-text", function() {
     $(this).css({
         width: outerW+'px'
     })
+});
+
+$('#parentContainer').on('mouseenter', '.default-text', function(){
+    curClass = $(this).attr('class').split(' ').pop();
+    $('#'+curClass).find('.section-title').addClass('hover');
+})
+
+$('#parentContainer').on('mouseleave', '.default-text', function(){
+    $('.section-title').removeClass('hover');
+})
+$('#parentContainer').on('change', '.img-src-input', function(){
+    console.log($(this).val());
+    curClass = $(this).parents('.imgEditor').attr('id')
+    console.log(curClass);
+    $('.'+curClass).find('.img-manip').attr('src', 'img/gallery/yeezy_1.jpg');
+
+})
+
+var save, saveX, saveY, saveText, saveId, saveWidth;
+
+
+$('#parentContainer').on("click", ".save-mod", function() {
+    saveX = 'left:'+ $(this).parents('.textEditor').find('.x-axis').val();
+    saveY = 'top:'+ $(this).parents('.textEditor').find('.y-axis').val();
+    saveId = 'id:'+ $(this).parents('.textEditor').attr('id');
+    saveText = 'text:'+ $(this).parents('.textEditor').find('.custom-text').val();
+
+
+    console.log(saveX + ' '+ saveY + ' '+ saveId+ ' '+ saveText);
 });
